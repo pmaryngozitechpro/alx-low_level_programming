db861dc816a2d1d6c180e37c34b81195bb4602d1
pmaryngozitechpro <pmaryngozitech@gmail.com>
2023-04-23 20:50:11 -0700

    comb2

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,14 +9,11 @@ int main(void)
 {
 	int n;
 
-	for (n = 48; n < 58; n++)
+	for (n = 48; n < 57; n++)
 	{
 		putchar(n);
-		if (n != 57)
-		{
-			putchar('.');
-			putchar(' ');
-		}
+		putchar('.');
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
