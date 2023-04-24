b722a6c1f745301a26b04c6aabc83e55cd4fdec2
pmaryngozitechpro <pmaryngozitech@gmail.com>
2023-04-23 21:18:18 -0700

    reverse

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,11 +9,14 @@ int main(void)
 {
 	int n;
 
-	for (n = 48; n < 57; n++)
+	for (n = '0'; n <= '9'; n++)
 	{
 		putchar(n);
+		if (n != '9')
+		{
 		putchar('.');
 		putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
